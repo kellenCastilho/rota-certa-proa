98be89acca23dcf088eb91ba93f4a8173f07676e
kellen castilho <kellencastilho@MacBook-Air-de-kellen.local>
2026-08-14 09:06:41 -0300

    Ativa analytics de visitantes

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import 'leaflet/dist/leaflet.css'
 import App from './App'
 import './styles/tokens.css'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter><App /></BrowserRouter>
+    <Analytics />
   </React.StrictMode>,
 )
